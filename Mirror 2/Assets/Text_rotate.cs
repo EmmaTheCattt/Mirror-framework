05fd25e834a23fc65c8c79241f9b8fbc026d8379
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-04-22 07:14:06 +0200

    Coolness 100

--- Mirror 2/Assets/Text_rotate.cs
+++ Mirror 2/Assets/Text_rotate.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Text_rotate : MonoBehaviour
 {
+
+    public SaveManager SaveData;
+    public TMP_Text name_text;
     public Transform Look_cam;
     public Vector3 offset = new Vector3(0, 180f, 0);
 
@@ -11,6 +16,8 @@ public class Text_rotate : MonoBehaviour
     void Start()
     {
         Look_cam = GameObject.Find("Main Camera").GetComponent<Transform>();
+        SaveData = GameObject.Find("SaveManager").GetComponent<SaveManager>();
+        name_text = GameObject.FindWithTag("PlayerName").GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
@@ -18,5 +25,6 @@ public class Text_rotate : MonoBehaviour
     {
         transform.LookAt(Look_cam);
         transform.Rotate(offset);
+        name_text.text = SaveData.saveFile.name;
     }
 }
