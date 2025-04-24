2d7e377c15e1d36f3b63bcbbaab8bdb5065abb1d
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-04-24 10:28:21 +0200

    No more now ;w;

--- Mirror 2/Assets/Scripts/ShipController.cs
+++ Mirror 2/Assets/Scripts/ShipController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using UnityEngine.AI;
+using Unity.VisualScripting;
 
 public class ShipController : NetworkBehaviour
 {
@@ -12,7 +13,7 @@ public class ShipController : NetworkBehaviour
 
     public bool Player_on = false;
 
-    public GameObject The_player;
+    public GameObject The_player = null;
 
     public Vector3 col_size;
 
@@ -20,7 +21,6 @@ public class ShipController : NetworkBehaviour
     void Start()
     {
         col_size = col.transform.localScale;
-        
     }
 
     // Update is called once per frame
@@ -35,30 +35,33 @@ public class ShipController : NetworkBehaviour
             col.transform.localScale = col_size;
         }
 
-        if (The_player.GetComponent<NetworkIdentity>().isLocalPlayer == true && The_player != null && Player_on == true)
+        if (The_player != null && Player_on == true)
         {
-            The_player.GetComponent<NavMeshAgent>().enabled = false;
-            The_player.transform.position = new Vector3(transform.position.x, -100, transform.position.z);
-            The_player.GetComponent<PlayerController>().On_ship = true;
-            The_player.GetComponent<PlayerController>().camera_player.transform.position = new Vector3(transform.position.x, transform.position.y + 13.25f, transform.position.z - 10);
+            if (The_player.GetComponent<NetworkIdentity>().isLocalPlayer == true)
+            {
+                The_player.GetComponent<NavMeshAgent>().enabled = false;
+                The_player.transform.position = new Vector3(transform.position.x, -100, transform.position.z);
+                The_player.GetComponent<PlayerController>().On_ship = true;
+                The_player.GetComponent<PlayerController>().camera_player.transform.position = new Vector3(transform.position.x, transform.position.y + 13.25f, transform.position.z - 10);
 
-            if (transform.position.y < 10)
-            {
-                transform.position += new Vector3(0, 0.1f * Time.deltaTime, 0);
-            }
-            else
-            {
-                transform.position += new Vector3(0, 10, 0);
+                if (transform.position.y < 10)
+                {
+                    transform.position += new Vector3(0, 1f * Time.deltaTime, 0);
+                }
+                else
+                {
+                    transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+                }
             }
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "Player")
+        if (other.CompareTag("Player"))
         {
             Player_on = true;
-            The_player = collision.gameObject;
+            The_player = other.gameObject;
         }
     }
 }
